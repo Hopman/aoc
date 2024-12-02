238c374946c777ff542af0e40f59c4fa3d7133f6
Hopman <hopman@users.noreply.github.com>
2024-12-02 10:15:13 +0100

    1.2: That's the right answer

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,4 @@
-use std::{cmp::{max, min}, fs::read_to_string};
+use std::fs::read_to_string;
 
 fn main() {
 
@@ -10,11 +10,9 @@ fn main() {
     let left_list_int: Vec<u64> = left_list_string.lines().map(|l| l.parse::<u64>().unwrap()).collect();
     let right_list_int: Vec<u64> = right_list_string.lines().map(|l| l.parse::<u64>().unwrap()).collect();
     
-    for (n, left) in left_list_int.iter().enumerate() {
-        let right = right_list_int[n];
-        let diff = max(left, &right) - min(left, &right);
-        result += diff;
-
+    for left in left_list_int.iter() {
+        let right_count = right_list_int.iter().filter(|r| *r == left).count() as u64;
+        result += left * right_count;
     }
 
     println!("Result: {result}");
