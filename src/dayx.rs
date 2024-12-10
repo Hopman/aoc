6adab650f5554fd4e33c5db32f09cc9621788b8a
Hopman <hopman@users.noreply.github.com>
2024-12-10 09:28:37 +0100

    [fix] Unwrap read to string in template

--- src/dayx.rs
+++ src/dayx.rs
@@ -2,7 +2,7 @@ use std::fs::read_to_string;
 
 pub fn dayx(inputpath: &str) -> usize {
     let mut result = 0;
-    let text = read_to_string(inputpath);
+    let text = read_to_string(inputpath).unwrap();
 
     return result;
 }
