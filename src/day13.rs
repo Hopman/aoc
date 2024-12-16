7f1c12c09b7e07a25d547849195047e7613e8e4e
Hopman <hopman@users.noreply.github.com>
2024-12-16 20:03:59 +0100

    13.2 That's the right answer!
    - Cramer rules!
    - No floating

--- src/day13.rs
+++ src/day13.rs
@@ -1,4 +1,4 @@
-use std::{fs::read_to_string, usize};
+use std::fs::read_to_string;
 
 pub fn day13(inputpath: &str) -> usize {
     let mut tokens = 0;
@@ -7,11 +7,9 @@ pub fn day13(inputpath: &str) -> usize {
 
     let lines: Vec<&str> = text.lines().collect();
 
-    for (i, l) in lines.chunks(4).enumerate() {
-        dbg!(i);
+    for l in lines.chunks(4) {
         let mut machine = Machine::from_input(l);
         tokens += machine.solve();
-        //dbg!(machine);
     }
 
     return tokens;
@@ -22,47 +20,22 @@ struct Machine {
     a: Button,
     b: Button,
     p: Point,
-    max: usize,
 }
 
 impl Machine {
     fn solve(&mut self) -> usize {
-        let mut solutions = Vec::new();
+        let a = ((self.p.x * self.b.y) as f64 - (self.b.x * self.p.y) as f64)
+            / ((self.a.x * self.b.y) as f64 - (self.b.x * self.a.y) as f64);
+        let b = ((self.a.x * self.p.y) as f64 - (self.p.x * self.a.y) as f64)
+            / ((self.a.x * self.b.y) as f64 - (self.b.x * self.a.y) as f64);
 
-        let button_matrix = nalgebra::Matrix2::new(
-            self.a.x as f64,
-            self.b.x as f64,
-            self.a.y as f64,
-            self.b.y as f64,
-        );
-
-        let prize_vector = nalgebra::Vector2::new(self.p.x as f64, self.p.y as f64);
-
-        let button_matrix_lu = button_matrix.lu();
-
-        let solution = button_matrix_lu.solve(&prize_vector);
-
-        if let Some(val) = solution {
-            let v: Vec<usize> = val.iter().filter(|&v| (v.round() - v).abs() < 1e-10).map(|&v| v as usize).collect() ;
-            dbg!(&val, &v);
-            if v.len() > 1 {
-                solutions.push(v[0] * self.a.cost + v[1] * self.b.cost);
-            }
-
+        if a.fract() != 0.0 || b.fract() != 0.0 {
+            return 0;
         }
 
-        //for n in 0..=100_000 {
-        //    for m in 0..=100_000 {
-        //        if n * self.a.x + m * self.b.x == self.p.x
-        //            && n * self.a.y + m * self.b.y == self.p.y
-        //        {
-        //            solutions.push(n * self.a.cost + m * self.b.cost);
-        //        }
-        //    }
-        //}
-
-        return solutions.into_iter().min().unwrap_or(0);
+        return a as usize * 3 + b as usize;
     }
+
     fn from_input(input: &[&str]) -> Self {
         let mut a_split = input[0].split_whitespace();
         let ax = a_split.next().unwrap().parse::<usize>().unwrap();
@@ -82,18 +55,15 @@ impl Machine {
             a: Button {
                 y: ay,
                 x: ax,
-                cost: 3,
             },
             b: Button {
                 y: by,
                 x: bx,
-                cost: 1,
             },
             p: Point {
                 y: prizey,
                 x: prizex,
             },
-            max: 100,
         }
     }
 }
@@ -102,7 +72,6 @@ impl Machine {
 struct Button {
     y: usize,
     x: usize,
-    cost: usize,
 }
 
 #[derive(Debug)]
@@ -119,14 +88,9 @@ mod tests {
         let result_example = day13("input/day13_example.txt");
         assert_eq!(result_example, 480);
     }
-    //#[test]
-    //fn day13_example_two_test() {
-    //    let result_example = day13::day13("input/day13_example_two.txt");
-    //    assert_eq!(result_example, 1306);
-    //}
     #[test]
     fn day13_test() {
         let result = day13("input/day13.txt");
-        assert_eq!(result, 870202);
+        assert_eq!(result, 40069);
     }
 }
